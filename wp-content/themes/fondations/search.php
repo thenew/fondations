7038b69b1f6b51b01513ac28062280b1f7c4b704
thenew <remy@thenew.fr>
2012-10-15 01:05:54 +0200

    AJAX Search

--- wp-content/themes/fondations/search.php
+++ wp-content/themes/fondations/search.php
@@ -1,1 +1,23 @@
-<?php require(TEMPLATEPATH.'/tpl/page.php');
+<?php
+if(isset($_GET["ajax"]) && "1" == $_GET["ajax"]) {
+    if(have_posts()):
+        $response = '{"posts": [';
+        $i = 0;
+        while(have_posts()): the_post();
+            if($i > 0) $response .= ',';
+            $response .= '{
+                "title": "'.get_the_title().'",
+                "permalink": "'.get_permalink().'"
+            }';
+            $i++;
+        endwhile;
+        $response .= ']}';
+    else:
+        $response = '{"error": "No posts"}';
+    endif;
+    echo $response;
+} else {
+    get_header();
+    require_once TEMPLATE_PATH.'/tpl/posts.php';
+    get_footer();
+}
