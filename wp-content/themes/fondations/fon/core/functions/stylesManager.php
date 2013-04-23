53b90af802238db042fe5342c4d74258cbc9d144
thenew <remy@thenew.fr>
2013-04-24 00:57:14 +0200

    stylesManager + switch plugins minify JS

--- wp-content/themes/fondations/fon/core/functions/stylesManager.php
+++ wp-content/themes/fondations/fon/core/functions/stylesManager.php
@@ -31,15 +31,25 @@ function fon_styles_init() {
             wp_enqueue_style(basename($file,'.css'),$file,array(),'1','all');
         }
     }else {
-        $css_min = '';
+        $css_content = '';
+        $public_dir = PUBLIC_PATH;
         // Inclusion des fichiers dans l'ordre
         foreach( $files as $file )
-            $css_min .= file_get_contents($file);
+            $css_content .= file_get_contents($file);
         // Compression du CSS
-        $css_min = compress_css($css_min);
-        // Mis en cache fichier externe
-        file_put_contents(PUBLIC_PATH.'/min.css', $css_min);
-
-        wp_enqueue_style('min',PUBLIC_URL.'/min.css',array(),'1','all');
+        $css_content = compress_css($css_content);
+        // version
+        $css_filename = 'min.css';
+        if(!get_option('fon_min_css_version')) {
+            if(!is_dir($public_dir))
+                @mkdir($public_dir);
+            @chmod($public_dir, 0777);
+            file_put_contents(PUBLIC_PATH.'/'.$css_filename, $css_content);
+            $css_version = time();
+            update_option('fon_min_css_version', $css_version);
+        } else {
+            $css_version = get_option('fon_min_css_version');
+        }
+        wp_enqueue_style('fon-min', PUBLIC_URL.'/'.$css_filename, array(), $css_version, 'all');
     }
 }
