63872bdb634692681e1c9f05d71eb48433dc4e5f
Rémy <remy@colorz.fr>
2014-01-24 14:24:45 +0100

    Enable WP automatic updates despite VCS

--- wp-content/themes/fondations/fon/core/system/general.php
+++ wp-content/themes/fondations/fon/core/system/general.php
@@ -4,3 +4,12 @@ add_filter('jpeg_quality', 'fon_jpeg_quality');
 function fon_jpeg_quality($arg) {
     return FON_JPEG_QUALITY;
 }
+
+// use WP automatic updates despite version control
+// http://make.wordpress.org/core/2013/10/25/the-definitive-guide-to-disabling-auto-updates-in-wordpress-3-7/
+function fon_automatic_update_vcs( $checkout, $context ) {
+    // if ( $context == ABSPATH )
+        return false;
+    // return $checkout;
+}
+add_filter( 'automatic_updates_is_vcs_checkout', 'fon_automatic_update_vcs', 10, 2 );
